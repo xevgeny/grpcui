a0618752b8f15ece274d0f628395843a3b4c5698
xevgeny <e.tskhovrebov@pm.me>
2019-10-01 13:22:02 +0200

    Support multiple targets

--- internal/resources/standalone/assets.go
+++ internal/resources/standalone/assets.go
@@ -1,7 +1,12 @@
 package standalone
 
 const IndexTemplateName = "index-template.html"
+const ListTemplateName = "list-template.html"
 
 func IndexTemplate() []byte {
 	return MustAsset(IndexTemplateName)
+}
+
+func ListTemplate() []byte {
+	return MustAsset(ListTemplateName)
 }
